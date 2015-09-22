dea3170ea1dc494af2b38c6575a5f09c39893203
Lucas Lindstrom <lucas.lindstrom@cinnober.com>
2015-09-22 15:37:51 +0200

    Improved logging facilities for jirafilters and added support for logging from the python script.

--- retrieveFilters.py
+++ retrieveFilters.py
@@ -5,30 +5,58 @@ username = sys.argv[2]
 password = sys.argv[3]
 filterId = sys.argv[4]
 output = sys.argv[5]
+logFilename = sys.argv[6]
 
 # fields: key, created, priority, status
 
+logFile = None
+
 def request(url):
+    unit = "retrieveFilters.request"
+
+    log(unit, "Sending request %s" % url)
+
     base64str = base64.b64encode(("%s:%s" % (username, password)).encode()).decode("ascii")
     headers = { "Authorization" : "Basic %s" % base64str }
     request = urllib.request.Request(url, headers=headers)
     gcontext = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
     response = urllib.request.urlopen(request, context=gcontext)
     encodedJson = response.read()
+
+    log(unit, "Received response %s" % encodedJson)
+
     decodedJson = encodedJson.decode("utf-8")
-    return json.loads(decodedJson)
+    result = json.loads(decodedJson)
+
+    log(unit, "Decoded and parsed as %s" % result)
+
+    return result
 
 def getFilter(id):
+    unit = "retrieveFilters.getFilter"
+
     url = "%s/rest/api/2/filter/%s" % (siteUrl, id)
+
+    log(unit, "Sending getFilter(id = %s)" % id)
+
     return request(url)
 
 def search(filterId):
+    unit = "retrieveFilters.search"
+
     url = "%s&fields=key,created,priority,status" % getSearchUrlByFilter(filterId)
+
+    log(unit, "Sending search(filterId = %s)" % filterId)
+
     return request(url)
 
 def getSearchUrlByFilter(filterId):
+    unit = "retrieveFilters.getSearchUrlByFilter"
+
     filter = getFilter(filterId)
-    print(filter)
+   
+    log(unit, "Extracted search URL %s from filter ID %s" % (filter["searchUrl"], filterId))
+
     return filter["searchUrl"]
 
 def jsonIssuesListToCsv(issuesList):
@@ -38,10 +66,27 @@ def jsonIssuesListToCsv(issuesList):
 
     return "\n".join(parsedIssueList)
 
+def log(unit, message):
+    now = time.strftime("%Y-%m-%d %H:%M:%S")
+    output = "[%s] %s: %s" % (now, unit, message)
+    print(output)
+    logFile.write("%s\n" % output)
+
+def openLogFile(filename):
+    global logFile
+    logFile = open(filename, "a")
+
+def closeLogFile():
+    logFile.close()
+
+unit = "retrieveFilters"
+
+openLogFile(logFilename)
 f = open(output, 'w')
 outputText = jsonIssuesListToCsv(search(filterId))
-print(outputText)
+log(unit, "Writing to output file: %s" % outputText)
 f.write(outputText)
 f.close()
+closeLogFile()
     
     
